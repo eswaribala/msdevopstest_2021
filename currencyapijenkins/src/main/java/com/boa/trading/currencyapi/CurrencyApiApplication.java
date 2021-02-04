e5a83c3f50bcdbb3f4ee5c9ce9f4d5486afdb36b
eswaribala <parameswaribala@gmail.com>
2021-02-04 23:26:45 +0530

    modified main appplication

--- currencyapijenkins/src/main/java/com/boa/trading/currencyapi/CurrencyApiApplication.java
+++ currencyapijenkins/src/main/java/com/boa/trading/currencyapi/CurrencyApiApplication.java
@@ -1,5 +1,6 @@
 package com.boa.trading.currencyapi;
 
+//testing the pipeline
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
